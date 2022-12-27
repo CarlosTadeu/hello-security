93b03ad0f94481dc39743e534672f0006f81d301
Carlos Tadeu <carlossantos@opus-software.com.br>
2022-12-27 11:18:58 -0300

    feat(security): add a custom username and password

--- src/main/java/com/carlostadeu/hellosecurity/HelloSecurityApplication.java
+++ src/main/java/com/carlostadeu/hellosecurity/HelloSecurityApplication.java
@@ -1,13 +1,48 @@
 package com.carlostadeu.hellosecurity;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 public class HelloSecurityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HelloSecurityApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSecurityApplication.class, args);
+    }
 
+}
+
+@RestController
+class GreetingHttpController {
+
+    @GetMapping("/greetings/{name}")
+    Greeting greet(@PathVariable String name) {
+        var good = StringUtils.hasText(name) && Character.isUpperCase(name.charAt(0));
+        if (!good) {
+            throw new IllegalArgumentException("the name must start with a capital letter");
+        }
+        return new Greeting("Hello, " + name + "!");
+    }
+}
+
+@ControllerAdvice
+@Slf4j
+class ProblemDetailErrorHandlingControllerAdvice {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    ProblemDetail onException(HttpServletRequest request) {
+        request.getAttributeNames().asIterator()
+                .forEachRemaining(attributeName -> log.info("attributeName" + attributeName));
+        return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "the name is invalid");
+    }
+}
+
+record Greeting(String message) {
 }
--- src/test/java/com/carlostadeu/hellosecurity/HelloSecurityApplicationTests.java
+++ src/test/java/com/carlostadeu/hellosecurity/HelloSecurityApplicationTests.java
@@ -1,13 +1,19 @@
 package com.carlostadeu.hellosecurity;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class HelloSecurityApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private GreetingHttpController greetingHttpController;
 
+    @Test
+    void contextLoads() {
+        assertThat(greetingHttpController).isNotNull();
+    }
 }
