6af17edd9fb876578da321b089a61c645ec32d42
Carlos Tadeu <carlossantos@opus-software.com.br>
2022-12-28 10:57:24 -0300

    test(security): add a spring security test with junit 5

---
+++ src/test/java/com/carlostadeu/hellosecurity/controllers/GreetingHttpControllerTest.java
@@ -0,0 +1,37 @@
+package com.carlostadeu.hellosecurity.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class GreetingHttpControllerTest {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @BeforeEach
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    void greet() throws Exception {
+        mvc.perform(get("/greetings/C").with(user("user").password("pass")))
+                .andExpect(status().isOk());
+    }
+}
