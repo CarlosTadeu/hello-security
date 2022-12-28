ee80250a36468edeec0f9a3b604ea897e8d389c0
Carlos Tadeu <carlossantos@opus-software.com.br>
2022-12-28 15:34:52 -0300

    test(security): add a security test with http basic

--- src/test/java/com/carlostadeu/hellosecurity/controllers/GreetingHttpControllerTest.java
+++ src/test/java/com/carlostadeu/hellosecurity/controllers/GreetingHttpControllerTest.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -30,8 +32,21 @@ class GreetingHttpControllerTest {
     }
 
     @Test
-    void greet() throws Exception {
-        mvc.perform(get("/greetings/C").with(user("user").password("pass")))
+    void greetWithRequestPostProcessor() throws Exception {
+        mvc.perform(get("/greetings/C").with(user("user")))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser("spring")
+    void greetWithAnnotation() throws Exception {
+        mvc.perform(get("/greetings/C"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void greetWithHttpBasic() throws Exception {
+        mvc.perform(get("/greetings/C").with(httpBasic("spring", "security")))
                 .andExpect(status().isOk());
     }
 }
