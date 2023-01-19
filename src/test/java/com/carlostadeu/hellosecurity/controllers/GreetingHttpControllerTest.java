d49d1c36b7e2418f9eaccdef7d5d2b451d18ab4f
Carlos Tadeu <carlossantos@opus-software.com.br>
2023-01-19 17:50:11 -0300

    refactor(imports): optimizing imports

--- src/test/java/com/carlostadeu/hellosecurity/controllers/GreetingHttpControllerTest.java
+++ src/test/java/com/carlostadeu/hellosecurity/controllers/GreetingHttpControllerTest.java
@@ -1,15 +1,12 @@
 package com.carlostadeu.hellosecurity.controllers;
 
-import com.carlostadeu.hellosecurity.config.SecurityConfig;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.context.support.WithUserDetails;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
